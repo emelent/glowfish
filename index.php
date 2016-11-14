feca233f3d85adeebeeeb375ac2e78317a6c7a51
Merrick Kavolsky <technicmk@gmail.com>
2016-11-14 19:40:32 +0200

    Add payfast btn link

--- index.php
+++ index.php
@@ -8,6 +8,8 @@
   <title>GlowFish</title>
 </head>
 <body>
-  <a href="https://glowfish.herokuapp.com/confirmation.php">Pay</a>
+  <a href="https://glowfish.herokuapp.com/confirmation.php">Pay</a><br/>
+  <a href="https://www.payfast.co.za/eng/process?cmd=_paynow&amp;receiver=11274014&amp;item_name=T1+Software&amp;item_description=Type+1+software+solution&amp;amount=100.00"><img src="https://www.payfast.co.za/images/buttons/dark-large-buynow.png" width="174" height="59" alt="Buy" title="Buy Now with PayFast" /></a>
+
 </body>
 </html>
