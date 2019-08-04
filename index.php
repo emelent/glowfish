9f70dc52207b398dd35cb42d5fd77886b5ebc22b
Roy Motloutsi <roy.motloutsi@gmail.com>
2019-08-04 19:32:23 +0200

    Update index.php

--- index.php
+++ index.php
@@ -10,6 +10,6 @@
 <body>
   <a href="https://glowfish.herokuapp.com/confirmation.php">Pay</a><br/>
   <a href="https://www.payfast.co.za/eng/process?cmd=_paynow&amp;receiver=11274014&amp;item_name=T1+Software&amp;item_description=Type+1+software+solution&amp;amount=100.00"><img src="https://www.payfast.co.za/images/buttons/dark-large-buynow.png" width="174" height="59" alt="Buy" title="Buy Now with PayFast" /></a>
-
+  <a href="https://glowfish.herokuapp.com/mail.php">Send Email</a><br/>
 </body>
 </html>
